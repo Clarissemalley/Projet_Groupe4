68d3ff636ac16a585c0f6addda14a0bd3d935a14
Alexandre Benalla <46560739+AlexandreBenalla@users.noreply.github.com>
2019-05-02 10:41:05 +0200

    Update main.py

--- main.py
+++ main.py
@@ -39,7 +39,6 @@ continuer= 1
 PA=99
 black = pygame.Color(0,0,0)
 smallfont = pygame.font.SysFont("comicsansms", 25)
-text= smallfont.render("PA: "+str(PA), True, black)
 numperso=1
 #</variables>
 
@@ -61,7 +60,6 @@ def selecperso(): #permet de changer de personnage utilisé en fonction de numpe
 
 
 def perso1(): #regroupe l'ensemble des événements disponibles quand on utilise le personnage 1
-        global PA
         eventperso1()
 
 def eventperso1():
@@ -85,7 +83,6 @@ def eventperso1():
                   listperso1[2]=listperso1[2]-1
 
 def perso2(): #regroupe l'ensemble des événements disponibles quand on utilise le personnage 2
-        global PA
         eventperso2()
 def eventperso2():
         global position_perso2
@@ -106,8 +103,7 @@ def eventperso2():
             if listperso2[2]>0 :
                 position_perso2 = position_perso2.move(-100,0)
                 listperso2[2]=listperso2[2]-1
-def perso3(): #regroupe l'ensemble des événements disponibles quand on utilise le personnage 3
-        global PA
+def perso3(): #regroupe l'ensemble des événements disponibles quand on utilise le personnage 
         eventperso3()
 def eventperso3():
         global position_perso3
@@ -129,7 +125,6 @@ def eventperso3():
                 position_perso3 = position_perso3.move(-100,0)
                 listperso3[2]=listperso3[2]-1
 def perso4(): #regroupe l'ensemble des événements disponibles quand on utilise le personnage 4
-        global PA
         eventperso4()
 def eventperso4():
         global position_perso4
@@ -173,4 +168,3 @@ while continuer :
     pygame.display.flip()
 
 pygame.quit()
-print(PA)
