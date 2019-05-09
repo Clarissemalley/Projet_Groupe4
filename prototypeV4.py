8734ed1a395e65dd5e89cd6101952fca5df35928
PoudreDePerlimpinpin <46561008+PoudreDePerlimpinpin@users.noreply.github.com>
2019-05-09 10:59:50 +0200

    Update prototypeV4.py

--- prototypeV4.py
+++ prototypeV4.py
@@ -5,9 +5,9 @@ from pygame.locals import *
 #initialisation de pygame
 pygame.init()
 
-#ouverture de la fenêtre pygame
-fenetre = pygame.display.set_mode((14*128, 8*128))
-plateau = pygame.Rect(0,0,14*128,8*128)
+#ouverture de la fenetre pygame
+fenetre = pygame.display.set_mode((11*128, 7*128))
+plateau = pygame.Rect(0,0,11*128,7*128)
 
 #chargement puis collage du fond
 fond = pygame.image.load("fond.gif").convert()
@@ -28,19 +28,19 @@ position_perso3 = position_perso3.move(0,4*128)
 
 sperso4 = pygame.image.load("Pretresse.gif").convert_alpha()
 position_perso4 = sperso4.get_rect()
-position_perso4 = position_perso4.move(2*128,6*128)
+position_perso4 = position_perso4.move(2*128,5*128)
 
 smechant1 = pygame.image.load("Draugr.gif").convert_alpha()
 position_mechant1 = smechant1.get_rect()
-position_mechant1 = position_mechant1.move(12*128,3*128)
+position_mechant1 = position_mechant1.move(10*128,1*128)
 
 smechant2 = pygame.image.load("Draugr.gif").convert_alpha()
 position_mechant2 = smechant2.get_rect()
-position_mechant2 = position_mechant2.move(12*128,1*128)
+position_mechant2 = position_mechant2.move(10*128,2*128)
 
 smechant3 = pygame.image.load("Draugr.gif").convert_alpha()
 position_mechant3 = smechant3.get_rect()
-position_mechant3 = position_mechant3.move(12*128,2*128)
+position_mechant3 = position_mechant3.move(10*128,3*128)
 
 
 
@@ -106,19 +106,19 @@ def interface(a):
         statsbd1=font.render("ATK: "+str(listperso4[4])+" SPD:"+str(listperso4[5]),1, black)
         statsbd2=font.render("ACC: "+str(listperso4[6])+" DEF: "+str(listperso4[7]),1, black)
         statsbd3=font.render("LCK: "+str(listperso4[8]),1, black)
-    fenetre.blit(statsbg1,(10,570))
-    fenetre.blit(statsbg2,(10,550))
-    fenetre.blit(statsbd1,(1500,570))
-    fenetre.blit(statsbd2,(1500,550))
-    fenetre.blit(statsbd3,(1500,530))
+    fenetre.blit(statsbg1,(0*128,6*128))       
+    fenetre.blit(statsbg2,(0*128,6*128+30)) 
+    fenetre.blit(statsbd1,(10*128-70,6*128))    #Permet de definir l'endroit où les stats s'affichent
+    fenetre.blit(statsbd2,(10*128-70,6*128+25))
+    fenetre.blit(statsbd3,(10*128-70,6*128+50))
 
-def perso1(): #regroupe l'ensemble des ÃƒÆ’Ã‚Â©vÃƒÆ’Ã‚Â©nements disponibles quand on utilise le personnage 1
+def perso1(): #regroupe l'ensemble des evenements disponibles quand on utilise le personnage 1
         eventperso1()
 
 def eventperso1():
-        list_perso = [position_perso1, position_perso2, position_perso3, position_perso4, position_mechant1, position_mechant2, position_mechant3]
         global position_perso1
         global listperso1
+        list_perso = [position_perso1, position_perso2, position_perso3, position_perso4, position_mechant1, position_mechant2, position_mechant3]
         if event.key == K_UP :
             if listperso1[2]>0 :
                 if position_perso1.move(0,-128).collidelist(list_perso) == -1 and plateau.contains(position_perso1.move(0,-128)) == 1:
@@ -140,12 +140,12 @@ def eventperso1():
                       position_perso1 = position_perso1.move(-128,0)
                       listperso1[2]=listperso1[2]-1
 
-def perso2(): #regroupe l'ensemble des ÃƒÆ’Ã‚Â©vÃƒÆ’Ã‚Â©nements disponibles quand on utilise le personnage 2
+def perso2(): #regroupe l'ensemble des evenements disponibles quand on utilise le personnage 2
         eventperso2()
 def eventperso2():
-        list_perso = [position_perso1, position_perso2, position_perso3, position_perso4, position_mechant1, position_mechant2, position_mechant3]
         global position_perso2
         global listperso2
+        list_perso = [position_perso1, position_perso2, position_perso3, position_perso4, position_mechant1, position_mechant2, position_mechant3]
         if event.key == K_UP :
               if listperso2[2]>0 :
                   if position_perso2.move(0,-128).collidelist(list_perso) == -1 and plateau.contains(position_perso2.move(0,-128)) == 1:
@@ -167,12 +167,12 @@ def eventperso2():
                     position_perso2 = position_perso2.move(-128,0)
                     listperso2[2]=listperso2[2]-1
 
-def perso3(): #regroupe l'ensemble des ÃƒÆ’Ã‚Â©vÃƒÆ’Ã‚Â©nements disponibles quand on utilise le personnage
+def perso3(): #regroupe l'ensemble des evenements disponibles quand on utilise le personnage
         eventperso3()
 def eventperso3():
-        list_perso = [position_perso1, position_perso2, position_perso3, position_perso4, position_mechant1, position_mechant2, position_mechant3]
         global position_perso3
-        global listperso3
+        global listperso3 
+        list_perso = [position_perso1, position_perso2, position_perso3, position_perso4, position_mechant1, position_mechant2, position_mechant3]
         if event.key == K_UP :
             if listperso3[2]>0 :
                 if position_perso3.move(0,-128).collidelist(list_perso) == -1 and plateau.contains(position_perso3.move(0,-128)) == 1:
@@ -194,12 +194,12 @@ def eventperso3():
                     position_perso3 = position_perso3.move(-128,0)
                     listperso3[2]=listperso3[2]-1
 
-def perso4(): #regroupe l'ensemble des ÃƒÆ’Ã‚Â©vÃƒÆ’Ã‚Â©nements disponibles quand on utilise le personnage 4
+def perso4(): #regroupe l'ensemble des evenements disponibles quand on utilise le personnage 4
         eventperso4()
 def eventperso4():
-        list_perso = [position_perso1, position_perso2, position_perso3, position_perso4, position_mechant1, position_mechant2, position_mechant3]
         global position_perso4
         global listperso4
+        list_perso = [position_perso1, position_perso2, position_perso3, position_perso4, position_mechant1, position_mechant2, position_mechant3]
         if event.key == K_UP :
             if listperso4[2]>0 :
                 if position_perso4.move(0,-128).collidelist(list_perso) == -1 and plateau.contains(position_perso4.move(0,-128)) == 1:
@@ -238,7 +238,7 @@ while continuer :
             selecperso()
     
 
-    fenetre.blit(fond, (0,0)
+    fenetre.blit(fond, (0,0))
     fenetre.blit(sperso1, position_perso1)
     fenetre.blit(sperso2, position_perso2)
     fenetre.blit(sperso3, position_perso3)
